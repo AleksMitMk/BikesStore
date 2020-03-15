8e7b064ba98dc01c25273f5ee6616c471927c599
aleksandar mitev <aleksandar.mitev.mk@gmail.com>
2020-03-15 22:23:10 +0100

    Updates

---
+++ precache-manifest.d1895e07186c0bcd2d1c2a78ca009744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0ca7d131fe13916cf017c6050b7880",
+    "url": "/Bike_Store/index.html"
+  },
+  {
+    "revision": "143cb1a0894299454d20",
+    "url": "/Bike_Store/static/css/main.16187ae9.chunk.css"
+  },
+  {
+    "revision": "ff54f6e6339fad1839a4",
+    "url": "/Bike_Store/static/js/2.2be5c075.chunk.js"
+  },
+  {
+    "revision": "143cb1a0894299454d20",
+    "url": "/Bike_Store/static/js/main.72bd6e90.chunk.js"
+  },
+  {
+    "revision": "9267646216289c68917b",
+    "url": "/Bike_Store/static/js/runtime-main.9f52627e.js"
+  },
+  {
+    "revision": "c13afca74f4fc9fe0170341487f60940",
+    "url": "/Bike_Store/static/media/logo.c13afca7.png"
+  }
+]);
